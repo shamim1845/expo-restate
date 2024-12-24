fb4107effc20ded71b4d08dc491e10973b7a6303
shamim1845 <samimraj1845@gmail.com>
2024-12-24 09:26:46 +0600

    Home screen added

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,18 +1,70 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-3xl my-10 font-rubik ">
-        Welcome to ReState
-      </Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView className="h-full" showsVerticalScrollIndicator={false}>
+        <View className="px-5">
+          <View className="flex flex-row items-center justify-between mt-5">
+            <View className="flex flex-row items-center">
+              <Image source={images.avatar} className="size-12 rounded-full" />
+              <View className="flex flex-col items-start ml-2 justify-center">
+                <Text className="text-xs font-rubik text-black-100">
+                  Good Morning
+                </Text>
+                <Text className="text-base font-rubik-medium text-black-300">
+                  Shamim
+                </Text>
+              </View>
+            </View>
+            <Image source={icons.bell} className="size-6" />
+          </View>
+          <Search />
+          {/* Featured  */}
+          <View className="mt-5">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">
+                Featured
+              </Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <View className="flex flex-row gap-5 mt-5">
+              <FeaturedCard />
+              <FeaturedCard />
+            </View>
+          </View>
+
+          {/* Recommendation */}
+          <View className="mt-5">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">
+                Our Recommendation
+              </Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <Filters />
+            <View className="flex flex-row gap-5 mt-5">
+              <Card />
+              <Card />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
